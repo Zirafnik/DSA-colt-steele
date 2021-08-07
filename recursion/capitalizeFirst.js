58ea301318782370d9c08a40578fe5d24cebe2e1
david <dh@ptica.com>
2021-08-07 16:26:09 +0200

    unfinished recursion algos

---
+++ recursion/capitalizeFirst.js
@@ -0,0 +1,12 @@
+//recursion
+//Ex. 19
+
+function capitalizeFirst(arr) {
+    let index = 0;
+
+    function helper(helperArr) {
+        if(index === helperArr.length) {
+            return;
+        }
+    }
+}
