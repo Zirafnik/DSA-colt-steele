d5b5c2e2f116a4f7cf242e65ee48f38df399fb45
david <dh@ptica.com>
2021-08-09 23:29:42 +0200

    optimized bubbleSort: check each iteration if sorted => if no swaps then == sorted & can return

--- sorting-algorithms/bubble-sort.js
+++ sorting-algorithms/bubble-sort.js
@@ -6,15 +6,20 @@ function swap(arr, idx1, idx2) {
 
 function bubbleSort(arr) {
     for(let j = arr.length; j > 0; j--) {
+        let sorted = true;
+
         for(let i = 0; i < j - 1; i++) {
             if(arr[i] > arr[i+1]) {
                 swap(arr, i, i+1);
+                sorted = false;
             }
         }
+
+        if(sorted) break;
     }
     return arr;
 }
 
 
-//console.log(bubbleSort([37, 45, 29, 8]));
+console.log(bubbleSort([37, 45, 29, 8]));
 console.log(bubbleSort([23,4,52,1,3,5,23,5,224,3,1,42,45,24,32,254,53,23,1,3224,42,4,2,3433,13,24343,53,2,3,2]));
