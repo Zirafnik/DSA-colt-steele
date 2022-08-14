2e0bd3e6503a117e9e754e2bfecdd3eee7019338
david <dh@ptica.com>
2022-08-14 18:34:05 +0200

    Ex.2 countUniqueValues solution

---
+++ problem-solving-patterns/Ex2_countUniqueValues.js
@@ -0,0 +1,22 @@
+// Ex. 2
+// multiple pointers? -> Solved it by using frequency.
+
+// Time: O(n)
+// Space: O(1)
+
+function countUniqueValues(arr) {
+    let count = 0;
+    let prevNum;
+
+    for(let i = 0; i < arr.length; i++) {
+        // skip same numbers
+        if(arr[i] === prevNum) continue;
+
+        prevNum = arr[i];
+        count++;
+    }
+
+    return count;
+}
+
+module.exports = countUniqueValues;
