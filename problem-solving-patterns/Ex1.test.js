eeee0bffec894f74cfd2afdeaffa774422d491bd
david <dh@ptica.com>
2022-08-14 18:07:48 +0200

    refactor: Ex.1 validAnagram solution

---
+++ problem-solving-patterns/Ex1.test.js
@@ -0,0 +1,32 @@
+const { validAnagram1, validAnagram2 } = require('./Ex1_validAnagram');
+
+describe('Ex.1: Valid anagram', () => {
+    describe('validAnagram1:', () => {
+        test('is valid', () => {
+            expect(validAnagram1('', '')).toEqual(true);
+        })
+        test('is NOT valid', () => {
+            expect(validAnagram1('aaz', 'zza')).toEqual(false);
+        })
+        test('is NOT valid', () => {
+            expect(validAnagram1('rat', 'car')).toEqual(false);
+        })
+        test('is valid', () => {
+            expect(validAnagram1('anagram', 'nagaram')).toEqual(true);
+        })
+    });
+    describe('validAnagram2:', () => {
+        test('is valid', () => {
+            expect(validAnagram2('', '')).toEqual(true);
+        })
+        test('is NOT valid', () => {
+            expect(validAnagram2('aaz', 'zza')).toEqual(false);
+        })
+        test('is NOT valid', () => {
+            expect(validAnagram2('rat', 'car')).toEqual(false);
+        })
+        test('is valid', () => {
+            expect(validAnagram2('anagram', 'nagaram')).toEqual(true);
+        })
+    });
+});
---
+++ problem-solving-patterns/Ex1_validAnagram.js
@@ -0,0 +1,64 @@
+//frequency counter
+//Ex. 1
+
+// Time: O(3n) => O(n)
+// Space: O(2) => O(1)
+function validAnagram1(str1, str2) {
+    if(str1.length !== str2.length) {
+        return false;
+    }
+
+    const str1Counters = {};
+    const str2Counters = {};
+
+    for(let char of str1) {
+        str1Counters[char] = str1Counters[char] ? ++str1Counters[char] : 1;
+    }
+
+    for(let char of str2) {
+        str2Counters[char] = str2Counters[char] ? ++str2Counters[char] : 1;
+    }
+
+    for(let key in str1Counters) {
+        if(!str2Counters[key]) return false;
+        if(str1Counters[key] !== str2Counters[key]) return false;
+    }
+    
+    return true;
+}
+
+// Time: O(2n) => O(n)
+// Space: O(1)
+function validAnagram2(str1, str2) {
+    if(str1.length !== str2.length) {
+        return false;
+    }
+
+    const freqCounters = {};
+
+    for(let char of str1) {
+        freqCounters[char] = freqCounters[char] ? ++freqCounters[char] : 1;
+    }
+
+    for(let char of str2) {
+        if(!freqCounters[char]) {
+            return false;
+        }
+        freqCounters[char]--;
+    }
+    
+    return true;
+}
+
+// console.log(validAnagram('', '') == true);
+// console.log(validAnagram('aaz', 'zza') == false);
+// console.log(validAnagram('anagram', 'nagaram') == true);
+// console.log(validAnagram('rat', 'car') == false);
+// console.log(validAnagram('awesome', 'awesom') == false);
+// console.log(validAnagram('qwerty', 'qeywrt') == true);
+// console.log(validAnagram('texttwisttime', 'timetwisttext') == true);
+
+module.exports = {
+    validAnagram1,
+    validAnagram2,
+};
