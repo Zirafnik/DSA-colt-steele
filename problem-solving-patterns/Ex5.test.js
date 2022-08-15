4f6227196db81269e57f7ff2532a2e4dea0b4e2b
david <dh@ptica.com>
2022-08-15 17:07:08 +0200

    Ex. 5 averagePair solution

---
+++ problem-solving-patterns/Ex5.test.js
@@ -0,0 +1,16 @@
+const averagePair = require('./Ex5_averagePair');
+
+describe('Ex.5: Average pair', () => {
+    test('contains pair', () => {
+        expect(averagePair([1,2,3],2.5)).toEqual(true);
+    })
+    test('contains pair', () => {
+        expect(averagePair([1,3,3,5,6,7,10,12,19],8)).toEqual(true);
+    })
+    test('does not contain pair', () => {
+        expect(averagePair([-1,0,3,4,5,6], 4.1)).toEqual(false);
+    })
+    test('does not contain pair', () => {
+        expect(averagePair([],4)).toEqual(false);
+    })
+});
---
+++ problem-solving-patterns/Ex5_averagePair.js
@@ -0,0 +1,27 @@
+// Ex. 5
+// Multiple pointers
+
+// Time: O(n)
+// Space: O(1)
+function averagePair(arr, targetAvg) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while(left < right) {
+        const tempAvg = (arr[left] + arr[right]) / 2;
+
+        if(tempAvg === targetAvg) {
+            return true;
+        }
+        if(tempAvg > targetAvg) {
+            right--;
+        }
+        if(tempAvg < targetAvg) {
+            left++;
+        }
+    }
+
+    return false;
+}
+
+module.exports = averagePair;
