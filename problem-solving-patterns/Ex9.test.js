49e97d8f7965da46524942520e7ae2c7e36f1681
david <dh@ptica.com>
2022-08-16 14:45:45 +0200

    Ex.9: findLongestSubstring solutions

---
+++ problem-solving-patterns/Ex9.test.js
@@ -0,0 +1,22 @@
+const findLongestSubstring = require('./Ex9_findLongestSubstring');
+
+describe('Ex.9: Find longest substring', () => {
+    test('empty', () => {
+        expect(findLongestSubstring('')).toEqual(0);
+    })
+    test('7', () => {
+        expect(findLongestSubstring('rithmschool')).toEqual(7);
+    })
+    test('6', () => {
+        expect(findLongestSubstring('thisisawesome')).toEqual(6);
+    })
+    test('7', () => {
+        expect(findLongestSubstring('thecatinthehat')).toEqual(7);
+    })
+    test('1', () => {
+        expect(findLongestSubstring('bbbbbb')).toEqual(1);
+    })
+    test('8', () => {
+        expect(findLongestSubstring('longestsubstring')).toEqual(8);
+    })
+});
