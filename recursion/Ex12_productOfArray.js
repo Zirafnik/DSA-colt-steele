7620161e0094b939dffa61909621f77b00fc2d31
david <dh@ptica.com>
2022-08-17 23:04:36 +0200

    Ex.12 productOfArray solutions

---
+++ recursion/Ex12_productOfArray.js
@@ -0,0 +1,26 @@
+// Ex. 12
+// Recursion
+
+// Time: O(n)
+// Space: O(1)
+function productOfArray(arr) {
+    if(arr.length === 0) return 1;
+
+    let last = arr.pop();
+
+    return last * productOfArray(arr);
+}
+
+// Solution with slice of the first element -> it also does not mutate the array
+// Time: O(n^2) ???
+// Space: O(1)
+function productOfArray(arr) {
+    if(arr.length <= 0) {
+        return 1;
+    }
+    
+    return arr[0] * productOfArray(arr.slice(1));
+}
+
+console.log(productOfArray([1,2,3])); // 6
+console.log(productOfArray([1,2,3,10])); // 60
