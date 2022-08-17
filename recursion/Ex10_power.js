3ffc95e5dc729a7234eb718e6944adca4e240306
david <dh@ptica.com>
2022-08-17 22:41:04 +0200

    Ex.10 power solution

---
+++ recursion/Ex10_power.js
@@ -0,0 +1,15 @@
+// Ex. 10
+// Recursion
+
+// Time: O(n)
+// Space: O(1)
+
+function power(base, exponent) {
+    if (exponent === 0) return 1;
+
+    return base * power(base, exponent - 1);
+}
+
+console.log(power(2,0)); // 1
+console.log(power(2,2)); // 4
+console.log(power(2,4)); // 16
