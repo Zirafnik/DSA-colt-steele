6395f80c88782d3d1e09a69e61c5f8e03088624d
david <dh@ptica.com>
2022-08-18 16:42:40 +0200

    Ex.16 isPalindrome solution

---
+++ recursion/Ex16_isPalindrome.js
@@ -0,0 +1,18 @@
+// Ex. 16
+// Recursion
+
+// Time: O(n)
+// Space: O(n)?
+function isPalindrome(str) {
+    if(str.length <= 1) return true;
+    if(str[0] !== str[str.length - 1]) return false;
+
+    return isPalindrome(str.slice(1, str.length-1));
+}
+
+console.log(isPalindrome('awesome')); // false
+console.log(isPalindrome('foobar')); // false
+console.log(isPalindrome('tacocat')); // true
+console.log(isPalindrome('amanaplanacanalpanama')); // true
+console.log(isPalindrome('amanaplanacanaspanama')); // false
+console.log(isPalindrome('amanaplanacanalpandemonium')); // false
