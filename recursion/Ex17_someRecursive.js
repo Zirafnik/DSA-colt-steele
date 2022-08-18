5a4b53b14eb0d33591051fab718b6572125a5a6a
david <dh@ptica.com>
2022-08-18 17:10:17 +0200

    Ex.17 someRecursive solution

---
+++ recursion/Ex17_someRecursive.js
@@ -0,0 +1,20 @@
+// Ex. 17
+// Recursion
+const isOdd = val => val % 2 !== 0;
+
+// Time: O(n)
+// Space: O(n) ?
+// modifies original passed in array because of pop -> use slice to make shallow copies
+function someRecursive(arr, cb) {
+    if(arr.length === 0) return false;
+    // if(cb(arr[arr.length - 1])) return true;
+    if(cb(arr.pop())) return true;
+
+    return someRecursive(arr, cb)
+    // return someRecursive(arr.slice(0, -1), cb);
+}
+
+console.log(someRecursive([1,2,3,4], isOdd)); // true
+console.log(someRecursive([4,6,8,9], isOdd)); // true
+console.log(someRecursive([4,6,8], isOdd)); // false
+console.log(someRecursive([4,6,8], val => val > 10)); // false
