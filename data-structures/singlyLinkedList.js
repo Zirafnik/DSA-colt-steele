bdcf8273c4711240003d4b0e024eadf765803200
david <dh@ptica.com>
2023-02-22 13:29:07 +0100

    singlyLinkedList: pop();

--- data-structures/singlyLinkedList.js
+++ data-structures/singlyLinkedList.js
@@ -27,9 +27,37 @@ class SinglyLinkedList {
 		// Returning this allows you to chain methods on the instance e.g. list.push().sthElse().push()
 		return this;
 	}
+
+	pop() {
+		if (this.length === 0) return;
+		if (this.length === 1) {
+			const poppped = this.head;
+
+			this.head = null;
+			this.tail = null;
+			this.length = 0;
+
+			return poppped;
+		}
+
+		let pre = this.head;
+		let last = this.head;
+
+		while (last.next) {
+			pre = last;
+			last = last.next;
+		}
+
+		pre.next = null;
+		this.tail = pre;
+		this.length -= 1;
+
+		return last;
+	}
 }
 
 const list = new SinglyLinkedList();
 list.push(3).push(6).push(1);
 
+console.log(list.pop());
 console.log(list);
