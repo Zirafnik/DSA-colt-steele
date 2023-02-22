383332b1fc3d631349126455f7973ce92d418cfe
david <dh@ptica.com>
2023-02-22 12:27:00 +0100

    partial: selectionSort

--- sorting-algorithms/basic/selection-sort.js
+++ sorting-algorithms/basic/selection-sort.js
@@ -1,27 +1,49 @@
+// Selection sort
+// Time: 
+// Space:
 function selectionSort(arr) {
-    let index = 0;
-    let min = Infinity;
-    let minIndex = 0;
+    for(let i = 0; i < arr.length; i++) {
+        let minVal = Infinity;
+        let minIdx = i;
 
-    while (index < arr.length) {
-        for(let j = index; j < arr.length; j++) {
-            let currentMin = Math.min(min, arr[j]);
-
-            if(min !== currentMin) {
-                min = currentMin;
-                minIndex = j;
+        for(let j = i + 1; j < arr.length; j++) {
+            if(arr[j] > minVal) {
+                minVal = arr[j];
+                minIdx = j;
             }
         }
-        //so we dont swap same indices
-        if(index !== minIndex) {
-            swap(arr, index, minIndex);  //could also do arr.indexOf(min) for the second arg here, but indexOf has to iterate through array again, and we avoid looking for the index with the above 'if' statement in 'for' loop
-        }
-        min = Infinity;
-        index++;
+        
+        swap(arr, i, minIdx);
     }
 
     return arr;
 }
+
+
+// function selectionSort(arr) {
+//     let index = 0;
+//     let min = Infinity;
+//     let minIndex = 0;
+
+//     while (index < arr.length) {
+//         for(let j = index; j < arr.length; j++) {
+//             let currentMin = Math.min(min, arr[j]);
+
+//             if(min !== currentMin) {
+//                 min = currentMin;
+//                 minIndex = j;
+//             }
+//         }
+//         //so we dont swap same indices
+//         if(index !== minIndex) {
+//             swap(arr, index, minIndex);  //could also do arr.indexOf(min) for the second arg here, but indexOf has to iterate through array again, and we avoid looking for the index with the above 'if' statement in 'for' loop
+//         }
+//         min = Infinity;
+//         index++;
+//     }
+
+//     return arr;
+// }
 
 function swap(arr, idx1, idx2) {
     let temp = arr[idx1];
@@ -30,6 +52,8 @@ function swap(arr, idx1, idx2) {
 }
 
 console.log(selectionSort([5, 3, 4, 1, 2]));
+console.log(selectionSort([37, 45, 29, 8]));
+console.log(selectionSort([23,4,52,1,3,5,23,5,224,3,1,42,45,24,32,254,53,23,1,3224,42,4,2,3433,13,24343,53,2,3,2]));
 
 //Time complexity: O(n^2);
 //Selection sort minimized number of swaps
